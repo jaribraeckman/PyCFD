51dbddd17c7ab47f5ce5c22394d50a49b611196f
Jari Braeckman <jari.braeckman@atlascopco.com>
2020-11-10 19:52:32 +0100

    Added gauss-seidel solver

--- Iterative Methods/iterative_methods.py
+++ Iterative Methods/iterative_methods.py
@@ -13,14 +13,14 @@ def jacobi(u, u_a, f, h, max_err, max_it):
         it = it + 1
         u_n[1:-1,1:-1] = 0.25*(u_n[2:,1:-1] + u_n[:-2,1:-1] + u_n[1:-1,2:] + u_n[1:-1,:-2] - f[1:-1,1:-1]*h*h)
 
-        err = help.error(u_n,u_a)
+        err = help.error(u_n, u_a)
 
-        conv = np.concatenate((conv,[err]))
+        conv = np.concatenate((conv, [err]))
 
         if err < max_err:
             break
 
-        if it > max_it:
+        if it >= max_it:
             break
 
     t = time.time() - t
@@ -32,3 +32,34 @@ def jacobi(u, u_a, f, h, max_err, max_it):
     help.plt2d(conv, it, 'Jacobi iteration method')
     return u_n, it, conv,t
 
+def gauss_seidel(u, u_a, f, h, max_err, max_it):
+    t = time.time()
+    conv = []
+    it = 0
+    u_n = u.copy()
+    nx, ny = np.shape(u)
+
+    while True:
+        it = it + 1
+        for i in range(1,nx-1):
+            for j in range(1,ny-1):
+                u_n[i,j] = 0.25*(u_n[i+1,j] + u_n[i-1,j] + u_n[i,j+1] + u_n[i,j-1] - f[i,j]*h*h)
+
+        err = help.error(u_n, u_a)
+
+        conv = np.concatenate((conv, [err]))
+
+        if err < max_err:
+            break
+
+        if it >= max_it:
+            break
+
+    t = time.time() - t
+
+    print('Computation time: ' + ('%.5f' %t) + 's')
+    print('Iterations: ', it)
+    print('Maximum error: ' + ('%.4f' %err))
+    help.plt2d(conv, it, 'Gauss-Seidel iteration method')
+
+    return u_n, it, conv, t
