4ad746b06a6c12c125f7fe0dae407f5ee90bd1a7
Jari Braeckman <jari.braeckman@atlascopco.com>
2020-11-16 15:51:04 +0100

    Added thomas algorithm

--- Iterative Methods/iterative_help.py
+++ Iterative Methods/iterative_help.py
@@ -15,3 +15,34 @@ def plt2d(err, it, title):
     fig.add_subplot(111)
     plt.plot(np.arange(1, it + 1), err)
     plt.title(title), plt.xlabel('iterations'), plt.ylabel('maximum error')
+
+# Create tridiagonal elements for thomas algorithm
+def CM(n):
+    # build coefficients matrix A (AX = B)
+    d = -4*np.ones(n) # main diagonal
+    l = np.ones(n-1) # lower diagonal
+    up = np.ones(n-1) # upper diagonal
+
+    d_n = d.copy() # modified diagonal
+    # d'[i] = d[i] - (l[i-1]/d[i-1])*up[i-1]
+    for i in range(1,n):
+        d_n[i] = d[i] - up[i-1]*l[i-1]/d_n[i-1]
+
+    return l, d_n, up
+
+# thomas algorithm
+def TDMA(B, l, d_n, n, up):
+    n = np.size(B)
+
+    # forward elimination of lower-diagonal elements
+    for i in range(1,N):
+        B[i] = B[i] - B[i-1]*l[i-1]/d_n[i-1]
+
+    X = np.zeros_like(B)
+
+    # backward substitution
+    X[-1] = B[-1]/d_n[-1]
+    for i in range(n-2,-1,-1):
+        X[i] = (B[i] - up[i]*X[i+1])/d_n[i]
+
+    return X
